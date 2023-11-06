d4fc68dc16be3819940220eef2b149e846266f0f
mehedi-hasans <amj.shazidhasan@gmail.com>
2023-11-06 13:02:25 +0600

    updated messages

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from .forms import RegistrationForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -9,8 +10,8 @@ def registrations(request):
         user_form=RegistrationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
+            messages.success(request,'Successfully registered')
            
-            return HttpResponse("successful registration")
     else:
         user_form=RegistrationForm()
     return render(request,'registration.html',{'user_form':user_form})    
