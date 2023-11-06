d45e77bb1061631be65bea0322455f14cf55d673
mehedi-hasans <amj.shazidhasan@gmail.com>
2023-11-06 12:49:38 +0600

    final commit form
    Custom registration with forms

--- project/app/views.py
+++ project/app/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render,HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-
+from .forms import RegistrationForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def registrations(request):
     if request.method == "POST":
-        user_form=UserCreationForm(request.POST)
+        user_form=RegistrationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
            
             return HttpResponse("successful registration")
     else:
-        user_form=UserCreationForm()
+        user_form=RegistrationForm()
     return render(request,'registration.html',{'user_form':user_form})    
